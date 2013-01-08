75a6d73552559410056c1460c11bc3eb2d6944a1
Matthew Szatmary <mszatmary@brightcove.com>
2013-01-08 11:03:22 -0800

    Some refactoring, Forced SELECT backend for maximum compatibility

--- Makefile
+++ Makefile
@@ -1,13 +1,5 @@
-OS := $(shell uname)
-ifeq ($(OS),Linux)
-EV_BACKEND += -DEV_USE_EPOLL
-endif
-ifeq ($(OS),Darwin)
-EV_BACKEND += -DEV_USE_KQUEUE
-endif
-
 all:
-	gcc -O3 $(EV_BACKEND) main.c -o zpv
+	gcc -O3 main.c -o zpv
 
 clean:
 	rm zpv
--- main.c
+++ main.c
@@ -22,17 +22,18 @@ limitations under the License.
 // #define EV_USE_POLL    1
 // #define EV_USE_EPOLL   1 // Linux Only
 // #define EV_USE_KQUEUE  1 // BSD/OSX Only
+#define EV_NO_THREADS 1
 
 #define EV_STANDALONE  1
 #include "ev.c"
 ////////////////////////////////////////////////////////////////////////////////
-#define BUFFER_SIZE (1*1024*1024)
+#define BUFFER_SIZE (4*1024*1024) // disk / fs blocks are uasually 4k
 struct ev_loop *loop;
-char data[BUFFER_SIZE];
+char data[BUFFER_SIZE+1];
 int data_size;
 
-#define WRITING 0
-#define READING 1
+#define READING 0
+#define WRITING 1
 int mode;
 
 typedef struct
@@ -53,11 +54,13 @@ static void print_timer()
     ev_tstamp total_time = now - start_time;
     if ( 0 == total_time ) return;
     fprintf(stderr, "{ \"stdin_wait_ms\": %d, \"stdout_wait_ms\": %d, \"total_time_ms\": %d, \"bytes_out\": %lld }\n",
-        (int)(1000*stdin_pipe.time_waiting), (int)(1000*stdout_pipe.time_waiting), (int)(1000*total_time), bytes_out );
+        (int)(1000 * ( stdin_pipe.time_waiting  + ( mode != READING ? 0 : now - stdin_pipe.timer_start ) ) ),
+        (int)(1000 * ( stdout_pipe.time_waiting + ( mode != WRITING ? 0 : now - stdout_pipe.timer_start) ) ),
+        (int)(1000 * total_time), bytes_out );
 }
 
-static void stdout_callback (ev_io *watcher, int revents);
-static void stdin_callback (ev_io *watcher, int revents)
+static void stdout_callback (EV_P_ ev_io *w, int revents);
+static void stdin_callback (EV_P_ ev_io *w, int revents)
 {
     if ( 0 == data_size )
     {
@@ -73,18 +76,21 @@ static void stdin_callback (ev_io *watcher, int revents)
         }
     }
 
-    // switch to write mode
-    mode = WRITING;
-    register double now = ev_now( loop );
-    stdout_pipe.timer_start = now;
-    stdin_pipe.time_waiting += now - stdin_pipe.timer_start;
-    ev_io_stop( loop, (ev_io*)&stdin_pipe ); // stop stdin
+    if( 0 < data_size )
+    {
+        // switch to write mode
+        register double now = ev_now( loop );
+        mode = WRITING;
+        stdout_pipe.timer_start = now;
+        stdin_pipe.time_waiting += now - stdin_pipe.timer_start;
 
-    ev_io_init ((ev_io*)&stdout_pipe, stdout_callback, STDOUT_FILENO, EV_WRITE);
-    ev_io_start( loop, (ev_io*)&stdout_pipe ); // start stdout
+        ev_io_init (&stdout_pipe.watcher, stdout_callback, STDOUT_FILENO, EV_WRITE);
+        ev_io_start( loop, &stdout_pipe.watcher ); // start stdout
+        ev_io_stop( loop, &stdin_pipe.watcher ); // stop stdin
+    }
 }
 
-static void stdout_callback (ev_io *watcher, int revents)
+static void stdout_callback (EV_P_ ev_io *w, int revents)
 {
     if ( 0 < data_size )
     {
@@ -99,15 +105,18 @@ static void stdout_callback (ev_io *watcher, int revents)
         data_size = 0;
     }
 
-    // Switch to read mode
-    mode = READING;
-    register double now = ev_now( loop );
-    stdin_pipe.timer_start = now;
-    stdout_pipe.time_waiting += now - stdout_pipe.timer_start;
-    ev_io_stop( loop, (ev_io*)&stdout_pipe );
+    if ( 0 == data_size )
+    {
+        // Switch to read mode
+        register double now = ev_now( loop );
+        mode = READING;
+        stdin_pipe.timer_start = now;
+        stdout_pipe.time_waiting += now - stdout_pipe.timer_start;
 
-    ev_io_init ((ev_io*)&stdin_pipe , stdin_callback , STDIN_FILENO , EV_READ);
-    ev_io_start( loop, (ev_io*)&stdin_pipe );
+        ev_io_init (&stdin_pipe.watcher, stdin_callback, STDIN_FILENO, EV_READ);
+        ev_io_start( loop, &stdin_pipe.watcher );
+        ev_io_stop( loop, &stdout_pipe.watcher );
+    }
 }
 
 static void timer_callback(struct ev_loop *loop, ev_timer *w, int revents)
@@ -125,7 +134,7 @@ static void timer_callback(struct ev_loop *loop, ev_timer *w, int revents)
     print_timer();
 }
 
-static void sigint_callback (ev_async *w, int revents)
+static void sigint_callback (struct ev_loop *loop, ev_signal *w, int revents)
 {
     print_timer();
     fprintf(stderr, "{ \"exit_status\": \"Success\", \"msg\": \"Received SIGINT\" }\n", data_size, errno);
@@ -134,10 +143,11 @@ static void sigint_callback (ev_async *w, int revents)
 
 int main(int argc, char **argv)
 {
+    int flags;
     mode = READING;
     data_size = 0;
     bytes_out = 0;
-    loop = ev_default_loop (0);
+    loop = ev_loop_new( EVBACKEND_SELECT );
     start_time = ev_time();
     ev_timer timer;
     ev_signal exitsig;
@@ -145,8 +155,9 @@ int main(int argc, char **argv)
     stdin_pipe.time_waiting = 0;
     stdin_pipe.timer_start = start_time;
 
-    ev_io_init ((ev_io*)&stdin_pipe , stdin_callback , STDIN_FILENO , EV_READ);
-    ev_io_start( loop, (ev_io*)&stdin_pipe );
+    ev_io_init (&stdout_pipe.watcher, stdout_callback, STDOUT_FILENO, EV_WRITE);
+    ev_io_init (&stdin_pipe.watcher, stdin_callback, STDIN_FILENO, EV_READ);
+    ev_io_start( loop, &stdin_pipe.watcher );
 
     ev_timer_init (&timer, timer_callback, 2.0, 2.0);
     ev_timer_start (loop, &timer);
